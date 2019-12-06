98f79daba9d0b5b52f90b932a61c692d11354f55
Mr. A <ag.perch@gmail.com>
2019-12-06 08:52:05 -0800

    Popops operational, timer working, quiz operational

--- script/script.js
+++ script/script.js
@@ -1,26 +1,133 @@
-console.log(questions);
-// for loop
-var questionsDeck = []
-questionsDeck = questArray(questions)
+var questionPop = document.getElementById("queMark")
+var submit = document.getElementById("submit")
+var timerOnPage = document.getElementById("timer")
+var options = document.getElementById("options")
+var optOne = document.getElementById("opt-one")
+var optTwo = document.getElementById("opt-two")
+var optThree = document.getElementById("opt-three")
+var optFour = document.getElementById("opt-four")
+var clickMe = document.querySelectorAll(".clickMe")
+var optBox = document.querySelector(".options-box")
+var firstPop = document.querySelector(".header")
+var secondPop = document.querySelector(".second-pop")
+var ChangeButton = document.querySelector("#change-over")
+var clicked = []
+var i = 0
+var timeLeft = questions.length * 15
 
-function questArray(arr) {
-    // Creating questions
-    var selectQue = []
-    for (i = 0; i < arr.length; i++) {
-        selectQue.push(arr[i].title)
-    }
-    console.log(selectQue);
-    // creating choices
-    var selectChoice = []
-    for (i = 0; i < arr.length; i++) {
-        selectChoice.push(arr[i].choices)
-    }
-    console.log(selectChoice);
-    // creating answers
-    var correctAns = []
-    for (i = 0; i < arr.length; i++) {
-        correctAns.push(arr[i].answer)
-    }
-    console.log(correctAns);
 
+function loadFirst() {
+  secondPop.style.display = "none"
 }
+
+ChangeButton.onclick = function () {
+  firstPop.style.display = "none"
+  secondPop.style.display = ""
+  console.log("show me");
+
+  questionsPage()
+  timer()
+}
+
+options.onclick = function () {
+  var answerSelect = []
+  clickMe.forEach(function (event) {
+    answerSelect.push(this.event.target.textContent)
+  })
+
+  if (answerSelect[0] === questions[i].answer) {
+    alert("You got the right answer! now close this")
+  } else {
+    alert("You got the wrong answer! you've been deducted some seconds! now close this")
+    timeLeft
+  }
+  i++
+  questionsPage()
+}
+
+function questionsPage() {
+  questionPop.textContent = questions[i].title
+  optOne.textContent = questions[i].choices[0]
+  optTwo.textContent = questions[i].choices[1]
+  optThree.textContent = questions[i].choices[2]
+  optFour.textContent = questions[i].choices[3]
+}
+
+function timer() {
+  var timeInterval = setInterval(function () {
+    timerOnPage.textContent = timeLeft;
+    timeLeft--;
+    if (timeLeft === 0) {
+      timerOnPage.textContent = "TIME UP";
+      clearInterval(timeInterval);
+    }
+  }, 1000);
+}
+loadFirst()
+
+
+
+
+
+
+
+// submit.addEventListener("click", function (event) {
+//     event.preventDefault()
+
+
+//     questionPop.textContent = questions[0].title
+
+//         questionPop.textContent = questions [qList].title
+
+
+
+// })
+
+// functional for loops for questions 
+
+      // optBox.addEventListener("click", function(event){
+      //   event.preventDefault()
+      //   for(j=0;j<clickMe.length;j++){
+      //     clicked.push(clickMe[j].innerText)
+      //   }
+      //   console.log(clicked);
+      //   })
+
+
+
+  //  for (let i = 0; i < clickMe.length; i++) {
+  //       clicked=clickMe[i]
+  //  }
+
+
+
+
+
+    //   optOne.addEventListener("click", function() {
+    //       if(questions[0+i].choices[0]===questions[0+i].answer){
+    //           console.log("YOU DID IT");
+    //       }else{
+    //           console.log("YOU SUCK");   
+    //       }
+    //   });
+    //   optTwo.addEventListener("click", function() {
+    //     if(questions[0+i].choices[1]===questions[0+i].answer){
+    //         console.log("YOY DID IT");
+    //     }else{
+    //         console.log("YOU SUCK");   
+    //     }
+    // });
+    // optThree.addEventListener("click", function() {
+    //     if(questions[0+i].choices[2]===questions[0+i].answer){
+    //         console.log("YOY DID IT");
+    //     }else{
+    //         console.log("YOU SUCK");   
+    //     }
+    // });
+    // optFour.addEventListener("click", function() {
+    //     if(questions[0+i].choices[3]===questions[0+i].answer){
+    //         console.log("YOY DID IT");
+    //     }else{
+    //         console.log("YOU SUCK");   
+    //     }
+    // })
--- script/test.js
+++ script/test.js
@@ -5,63 +5,24 @@ const questions = [
       answer: "alerts"
     },
     {
-      title: "",
-      choices: ["", "", "", ""],
-      answer: ""
+      title: "How many tags are in a regular HTML element?",
+      choices: ["1", "3", "2", "It depends"],
+      answer: "2" 
     },
     {
-      title: "",
-      choices: ["", "", "", ""],
-      answer: ""
+      title: "Which of the following is an example of an empty element?",
+      choices: ["< img / >", "< img > < / img >", "You are!", "< / img>"],
+      answer: "< img > < / img >"
     },
     {
-      title: "",
-      choices: ["", "", "", ""],
-      answer: ""
+      title: ".onclick , is it a JS or JQuery Property",
+      choices: ["None", "JQuery", "Both", "JS"],
+      answer: "JS"
     },
     {
-      title: "",
-      choices: ["", "", "", ""],
-      answer: ""
+      title: "In what do we believe?",
+      choices: ["ABC", "LMN", "OOO", "XYZ"],
+      answer: "ABC"
     },
-    {
-      title: "",
-      choices: ["", "", "", ""],
-      answer: ""
-    },
-    {
-      title: "",
-      choices: ["", "", "", ""],
-      answer: ""
-    },
-    {
-      title: "",
-      choices: ["", "", "", ""],
-      answer: ""
-    },
-    {
-      title: "",
-      choices: ["", "", "", ""],
-      answer: ""
-    },
-    {
-      title: "",
-      choices: ["", "", "", ""],
-      answer: ""
-    },
-    {
-      title: "",
-      choices: ["", "", "", ""],
-      answer: ""
-    },
-    {
-      title: "",
-      choices: ["", "", "", ""],
-      answer: ""
-    },
-    {
-      title: "",
-      choices: ["", "", "", ""],
-      answer: ""
-    }
+
 ]
